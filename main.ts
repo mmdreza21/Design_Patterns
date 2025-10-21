dc553de9a87e59595490c093faa0f7162ef79a71
mmdreza21 <mrezaj11@gmail.com>
2025-10-21 16:19:45 +0330

    refactor: update package.json and tsconfig.json for module support and dependencies
    - Changed author name in package.json.
    - Set type to module and main entry point in package.json.
    - Updated start script to use nodemon for development.
    - Removed unused dependencies and added nodemon as a dependency.
    - Updated TypeScript version in devDependencies.
    - Refactored tsconfig.json for better clarity and updated module settings.
    - Enabled strict type checking and skipped library checks in tsconfig.json.

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { TemplatePattern } from "./src/template-pattern/template-pattern.service.ts";
+
+class Main {
+  constructor() {
+    TemplatePattern.test();
+  }
+}
+
+const main = new Main();
